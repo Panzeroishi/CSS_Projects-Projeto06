2796b1241aeb3456f6afb298a8b9881fc7270171
Panzeroishi <diogooishi88@gmail.com>
2022-03-08 23:10:18 -0300

    primeiro commit no projeto 06

---
+++ script.js
@@ -0,0 +1,22 @@
+
+function mouseAcimaL(){
+    document.getElementById("linkedin").src ="img/linkedin_hover.png";
+}
+
+function mouseForaL(){
+    document.getElementById("linkedin").src ="img/linkedin.png"
+}
+
+function mouseAcimaI(){
+    document.getElementById("instagram").src ="img/instagram_hover.png"
+}
+function mouseForaI(){
+    document.getElementById("instagram").src ="img/instagram.png"
+}
+
+function mouseAcimaW(){
+    document.getElementById("wpp").src ="img/whatsapp_hover.png"
+}
+function mouseForaW(){
+    document.getElementById("wpp").src ="img/whatsapp.png"
+}
